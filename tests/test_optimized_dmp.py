ec4a8f16a20adcfc8a9feaf30ef6ada726870569
ngawangtrinley <ngawangtrinley@gmail.com>
2020-07-29 23:59:42 +0800

    test: convert generator to list

--- tests/test_optimized_dmp.py
+++ tests/test_optimized_dmp.py
@@ -7,11 +7,10 @@ from antx.utils import optimized_diff_match_patch
 def input_texts():
     return "Hello World.", "Goodbye World."
 
-
-def test_optimzed_dmp(input_texts):
+def test_optimized_dmp(input_texts):
     text1, text2 = input_texts
     dmp = optimized_diff_match_patch()
 
     diffs = dmp.diff_main(text1, text2)
 
-    assert diffs == [(-1, "Hell"), (1, "G"), (0, "o"), (1, "odbye"), (0, " World.")]
+    assert list(diffs) == [(-1, "Hell"), (1, "G"), (0, "o"), (1, "odbye"), (0, " World.")]
