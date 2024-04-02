ecd6a4f388ba7a03f519a51a5a7d55ac8ec1273e
Tenzin <ten13zin@gmail.com>
2024-04-02 11:34:25 +0530

    fix: upgrade pyyaml version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setuptools.setup(
     ],
     install_requires=[
         "diff-match-patch==20181111",
-        "PyYAML>=5.4, <6.0",
+        "PyYAML>=6.0",
         "requests>=2.24.0, <3.0",
     ],
     python_requires=">=3.8",
